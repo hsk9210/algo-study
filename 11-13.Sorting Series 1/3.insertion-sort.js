5084d60073fe0f4559b10bc062a1a5de0b3c7b98
Paul B Kim <50508394+pbkim0725@users.noreply.github.com>
2021-01-08 22:04:19 +0900

    added few more leetcodes

--- 11-13.Sorting Series 1/3.insertion-sort.js
+++ 11-13.Sorting Series 1/3.insertion-sort.js
@@ -6,47 +6,20 @@ Psuedocode
 * continue to the next element and if its in the incorrect order, iterate thrtough the second position(left side) to place the element in the correct place.
 
 */
-function swap(arr, idx1, idx2) {
-	let temp = arr[idx1];
-	arr[idx1] = arr[idx2];
-	arr[idx2] = temp;
-}
 
-function insertionSort(arr) {
-	// compare the second elem with the one before and swap if necessary
-	if (arr[1] < arr[0]) {
-		swap(arr, 0, 1);
-	}
-
-	// start by picking the second elem in the array
-	for (let i = 1; i < arr.length - 1; i++) {
-		if (arr[i + 1] < arr[i]) {
-			// iterate through sorted portion
-			for (let j = 0; j < i + 1; j++) {
-				// insert an element, where it belongs
-				if (arr[j] > arr[i + 1]) {
-					swap(arr, j, i + 1);
-				}
-			}
+const insertionSort = (arr) => {
+	for (let i = 1; i < arr.length; i++) {
+		let j = i - 1;
+		let tmp = arr[i];
+		while (j >= 0 && arr[j] > tmp) {
+			arr[j + 1] = arr[j];
+			console.log(j, arr);
+			j--;
 		}
+		arr[j + 1] = tmp;
+		console.log(`${i} round complete`, arr);
 	}
-
 	return arr;
-}
+};
 
 insertionSort([2, 1, 9, 89, 4]);
-// cv;               j  i
-// j;
-
-function insertionSort(arr) {
-	for (let i = 1; i < arr.length; i++) {
-		if (arr[i] > arr[i + 1]) {
-			for (let j = 0; j < i + 1; j++) {
-				if (arr[j] > arr[i + 1]) {
-					[arr[j], arr[i + 1]] = [arr[j + 1], arr[j]];
-				}
-			}
-		}
-	}
-	return arr;
-}
